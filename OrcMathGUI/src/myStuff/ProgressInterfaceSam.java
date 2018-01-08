20effa9130df6d1646b33eca0f37c745b3877428
samdundun <31856702+samdundun@users.noreply.github.com>
2018-01-08 00:08:02 -0500

    Update ProgressInterfaceSam.java

--- OrcMathGUI/src/myStuff/ProgressInterfaceSam.java
+++ OrcMathGUI/src/myStuff/ProgressInterfaceSam.java
@@ -3,5 +3,8 @@ package myStuff;
 import guiTeacher.interfaces.Visible;
 
 public interface ProgressInterfaceSam extends Visible {
-
+  void gameOver();
+	void setRound(int i);
+	void setSequenceSize(int i);
+  
 }
